ea724061f85840e4bd43ab5ccb567790b387debc
Hitmans-codes <154245536+Hitmans-codes@users.noreply.github.com>
2026-08-25 17:34:48 +0530

    Add emotion detection functionality with emotion_detector method

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,25 @@
+import json
+
+import requests
+
+
+def emotion_detector(text_to_analyze):
+    response = requests.post(
+        "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict",
+        headers={
+            "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+        },
+        json={"raw_document": {"text": text_to_analyze}},
+    )
+    formatted_response = json.loads(response.text)
+    emotion_scores = formatted_response["emotionPredictions"][0]["emotion"]
+
+    result = {
+        "anger": emotion_scores["anger"],
+        "disgust": emotion_scores["disgust"],
+        "fear": emotion_scores["fear"],
+        "joy": emotion_scores["joy"],
+        "sadness": emotion_scores["sadness"],
+    }
+    result["dominant_emotion"] = max(result, key=result.get)
+    return result
